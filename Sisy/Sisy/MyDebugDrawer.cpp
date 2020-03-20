e6adfced14a9db2fe1f159e3bee109cba56ef8aa
yangfeng <yangfengzzz@hotmail.com>
2020-03-20 08:23:17 +0800

    add MyDebugDrawer

---
+++ Sisy/Sisy/MyDebugDrawer.cpp
@@ -0,0 +1,43 @@
+//
+//  MyDebugDrawer.cpp
+//  Sisy
+//
+//  Created by 杨丰 on 2020/3/20.
+//  Copyright © 2020 杨丰. All rights reserved.
+//
+
+#include "MyDebugDrawer.hpp"
+#include "OgreManualObject2.h"
+#include "OgreSceneManager.h"
+
+namespace Demo{
+
+void MyDebugDrawer::flushLines()
+{
+    Ogre::SceneManager *sceneManager = m_glApp->getSceneManager();
+    int sz = m_linePoints.size();
+    if (sz)
+    {
+        Ogre::ManualObject * manualObject = sceneManager->createManualObject();
+        manualObject->begin("BaseWhite", Ogre::OT_LINE_LIST);
+        for (int i = 0; i < sz; i+=2) {
+            manualObject->position(m_linePoints[i].x,
+                                   m_linePoints[i].y,
+                                   m_linePoints[i].z);
+            manualObject->position(m_linePoints[i+1].x,
+                                   m_linePoints[i+1].y,
+                                   m_linePoints[i+1].z);
+            manualObject->line(m_lineIndices[i], m_lineIndices[i+1]);
+            manualObject->colour(m_currentLineColor.x(),
+                                 m_currentLineColor.y(),
+                                 m_currentLineColor.z(),
+                                 1.f);
+        }
+        manualObject->end();
+        
+        m_linePoints.clear();
+        m_lineIndices.clear();
+    }
+}
+
+}
---
+++ Sisy/Sisy/MyDebugDrawer.hpp
@@ -0,0 +1,117 @@
+//
+//  MyDebugDrawer.hpp
+//  Sisy
+//
+//  Created by 杨丰 on 2020/3/20.
+//  Copyright © 2020 杨丰. All rights reserved.
+//
+
+#ifndef MyDebugDrawer_hpp
+#define MyDebugDrawer_hpp
+
+#include "btBulletDynamicsCommon.h"
+#include "GraphicsSystem.h"
+
+namespace Demo{
+#define BT_LINE_BATCH_SIZE 512
+struct MyDebugVec3
+{
+    MyDebugVec3(const btVector3& org)
+        : x(org.x()),
+          y(org.y()),
+          z(org.z())
+    {
+    }
+
+    float x;
+    float y;
+    float z;
+};
+
+ATTRIBUTE_ALIGNED16(class)
+MyDebugDrawer : public btIDebugDraw
+{
+    GraphicsSystem* m_glApp;
+    int m_debugMode;
+
+    btAlignedObjectArray<MyDebugVec3> m_linePoints;
+    btAlignedObjectArray<unsigned int> m_lineIndices;
+
+    btVector3 m_currentLineColor;
+    DefaultColors m_ourColors;
+
+public:
+    BT_DECLARE_ALIGNED_ALLOCATOR();
+
+    MyDebugDrawer(GraphicsSystem * app)
+    : m_glApp(app),
+    m_debugMode(btIDebugDraw::DBG_DrawWireframe | btIDebugDraw::DBG_DrawAabb),
+    m_currentLineColor(-1, -1, -1)
+    {
+    }
+
+    virtual ~MyDebugDrawer()
+    {
+    }
+    virtual DefaultColors getDefaultColors() const
+    {
+        return m_ourColors;
+    }
+    ///the default implementation for setDefaultColors has no effect. A derived class can implement it and store the colors.
+    virtual void setDefaultColors(const DefaultColors& colors)
+    {
+        m_ourColors = colors;
+    }
+
+    virtual void drawLine(const btVector3& from1, const btVector3& to1, const btVector3& color1)
+    {
+        //float from[4] = {from1[0],from1[1],from1[2],from1[3]};
+        //float to[4] = {to1[0],to1[1],to1[2],to1[3]};
+        //float color[4] = {color1[0],color1[1],color1[2],color1[3]};
+        //m_glApp->m_instancingRenderer->drawLine(from,to,color);
+        if (m_currentLineColor != color1 || m_linePoints.size() >= BT_LINE_BATCH_SIZE)
+        {
+            flushLines();
+            m_currentLineColor = color1;
+        }
+        MyDebugVec3 from(from1);
+        MyDebugVec3 to(to1);
+
+        m_linePoints.push_back(from);
+        m_linePoints.push_back(to);
+
+        m_lineIndices.push_back(m_lineIndices.size());
+        m_lineIndices.push_back(m_lineIndices.size());
+    }
+
+    virtual void drawContactPoint(const btVector3& PointOnB, const btVector3& normalOnB, btScalar distance, int lifeTime, const btVector3& color)
+    {
+        drawLine(PointOnB, PointOnB + normalOnB * distance, color);
+        btVector3 ncolor(0, 0, 0);
+        drawLine(PointOnB, PointOnB + normalOnB * 0.01, ncolor);
+    }
+
+    virtual void reportErrorWarning(const char* warningString)
+    {
+    }
+
+    virtual void draw3dText(const btVector3& location, const char* textString)
+    {
+    }
+
+    virtual void setDebugMode(int debugMode)
+    {
+        m_debugMode = debugMode;
+    }
+
+    virtual int getDebugMode() const
+    {
+        return m_debugMode;
+    }
+
+    virtual void flushLines();
+};
+
+}
+
+#endif /* MyDebugDrawer_hpp */
